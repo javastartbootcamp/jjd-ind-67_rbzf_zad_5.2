5b64ce6210209edcc497a8db687af22d3eff6691
rbzf <anna.sadkowska@gmail.com>
2023-02-19 08:16:24 +0100

    Rozwiazanie. Format problem: jak zrobic, zeby wpisac stopien w formie °C, zeby checkstyle nie protestowal?

---
+++ src/main/java/pl/javastart/task/Room.java
@@ -0,0 +1,32 @@
+package pl.javastart.task;
+
+public class Room {
+    private final double surface;
+    private double temperature;
+    private final double minTemperature;
+    private final boolean airCondition;
+
+    public Room(double surface, double temperature, double minTemperature, boolean airCondition) {
+        this.surface = surface;
+        this.temperature = temperature;
+        this.minTemperature = minTemperature;
+        this.airCondition = airCondition;
+    }
+
+    String getInfo() {
+        String format = "powierzchnia pomieszczenia: %.2f m.kw., aktualna temperatura: %.1f\u00B0C, " +
+                "temperatura minimalna: %.1f\u00B0C, zamontowany klimatyzator: %b";
+        return format.formatted(surface, temperature, minTemperature, airCondition);
+    }
+
+    boolean decreaseTemp() {
+        if (airCondition && ((temperature - 1) >= minTemperature)) {
+            temperature--;
+            return true;
+        }   else if ((temperature - 1) < minTemperature) {
+            temperature = minTemperature;
+            return false;
+        }
+        return false;
+    }
+}
