f24dac9f461afa1b7c49deb9164f920751d91962
rbzf <anna.sadkowska@gmail.com>
2023-02-19 08:16:24 +0100

    Rozwiazanie poprawione.

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Room room = new Room(12, 23.5, 20, true);
+        System.out.println(room.getInfo());
+        System.out.println(room.decreaseTemp());
+        System.out.println(room.decreaseTemp());
+        System.out.println(room.decreaseTemp());
+        System.out.println("po zmianie:");
+        System.out.println(room.getInfo());
+
     }
 }
